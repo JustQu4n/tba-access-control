67fa5c36c1c6e85b1594f023dd8ae8dca60e6aa5
JustQu4n <nguyenanhquan01102002@gmail.com>
2025-08-13 11:22:36 +0700

    update role and permission

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -1,29 +1,43 @@
 // src/permissions/permissions.controller.ts
-import { Controller, Get, Post, Patch, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { PermissionGuard } from 'src/common/guards/permission.guard';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Permissions } from 'src/common/decorators/permissions.decorator';
 
 @Controller('permissions')
+@UseGuards(JwtAuthGuard, RoleGuard, PermissionGuard)
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
   @Get()
+  @Roles('Admin', 'SuperAdmin')
+  @Permissions('permission.read')
   findAll() {
     return this.permissionsService.findAll();
   }
 
   @Post()
+  @Roles('SuperAdmin')
+  @Permissions('permission.create')
   create(@Body() body: CreatePermissionDto) {
     return this.permissionsService.create(body);
   }
 
   @Patch(':id')
+  @Roles('SuperAdmin')
+  @Permissions('permission.update')
   update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdatePermissionDto) {
     return this.permissionsService.update(id, body);
   }
 
   @Delete(':id')
+  @Roles('SuperAdmin')
+  @Permissions('permission.delete')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.permissionsService.remove(id);
   }
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,29 +1,43 @@
 // src/roles/roles.controller.ts
-import { Controller, Get, Post, Patch, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { PermissionGuard } from 'src/common/guards/permission.guard';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Permissions } from 'src/common/decorators/permissions.decorator';
 
 @Controller('roles')
+@UseGuards(JwtAuthGuard, RoleGuard, PermissionGuard)
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Get()
+  @Roles('Admin', 'SuperAdmin')
+  @Permissions('role.read')
   findAll() {
     return this.rolesService.findAll();
   }
 
   @Post()
+  @Roles('SuperAdmin')
+  @Permissions('role.create')
   create(@Body() body: CreateRoleDto) {
     return this.rolesService.create(body);
   }
 
   @Patch(':id')
+  @Roles('SuperAdmin')
+  @Permissions('role.update')
   update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateRoleDto) {
     return this.rolesService.update(id, body);
   }
 
   @Delete(':id')
+  @Roles('SuperAdmin')
+  @Permissions('role.delete')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.rolesService.remove(id);
   }
